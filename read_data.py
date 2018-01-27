f0513845d25145002b19bcf1a117c8255fee363d
John Nduli <yohanaizraeli@gmail.com>
2018-01-27 10:46:54 +0300

    Add command line options to repo

--- read_data.py
+++ read_data.py
@@ -1,4 +1,8 @@
+"""
+Script for picking up data and analyzing best stocks to buy
+"""
 import re
+import argparse
 import pandas as pd
 
 
@@ -47,11 +51,46 @@ def remove_stocks_less_dividend(dividend, dataframe):
     """
     return dataframe.query('DPS >= %s' %dividend)
 
+def create_parser():
+    """
+    Creates the argument parser in use
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_price',
+                        help='Maximum Amount to spend on shares',
+                        default=5000, type=int)
+    parser.add_argument('--add_aims',
+                        help='Add Alternative Investment Stocks',
+                        action="store_true")
+    parser.add_argument('--add_loss_makers',
+                        help='Add loss making companies to result',
+                        action="store_true")
+    parser.add_argument('--minimum_dividend',
+                        help='Set the minimum divident to check. Defaults to 0',
+                        default=0,
+                        type=float)
+    parser.add_argument('--order_by_PE_DPS',
+                        help='Order results by P/E values and then DPS value',
+                        action='store_true')
+    args = parser.parse_args()
+    print_result(args.max_price, args.add_aims,
+                 args.add_loss_makers, args.minimum_dividend, args.order_by_PE_DPS)
 
+def print_result(max_price, add_aims, add_loss_makers, minimum_dividend, order_by_pe_dps):
+    """
+    Print table containing stocks that meet criteria
+    """
+    data = get_data_frame()
+    if add_aims is False:
+        data = remove_aims_stocks(data)
+    if add_loss_makers is False:
+        data = remove_negative_eps(data)
 
-dataf = get_data_frame()
-stocks_in_range = get_stocks_within_range(5000, dataf)
-non_negatives = remove_negative_eps(stocks_in_range)
-less_aims_stocks = remove_aims_stocks(non_negatives)
-divident_stocks = remove_stocks_less_dividend(0.5, less_aims_stocks)
-print(divident_stocks.to_string(index=False))
+    data = get_stocks_within_range(max_price, data)
+    data = remove_stocks_less_dividend(minimum_dividend, data)
+    if order_by_pe_dps:
+        data = data.sort_values(by=['P/E', 'DPS'], ascending=[True, False])
+    print(data.to_string(index=True))
+
+if __name__ == '__main__':
+    create_parser()
