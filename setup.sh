49739d1be45bfc63a528e6680e84fcd3f9cade82
John Nduli <yohanaizraeli@gmail.com>
2018-06-13 07:21:32 +0300

    Remove user in crontab setup

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ echo $SCRIPTPATH$RUNSCRIPT
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "*/30 9-15 * * * rookie $SCRIPTPATH$RUNSCRIPT" >> mycron
+echo "*/30 9-15 * * * $SCRIPTPATH$RUNSCRIPT" >> mycron
 #install new cron file
 crontab mycron
